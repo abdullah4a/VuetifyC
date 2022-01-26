76f503bb87e503e80dcfe2f3348aeed964ef7522
wa_ab <waqas@itinsync.com>
2022-01-26 19:53:24 +0500

    initial

---
+++ src/components/appbar/AppBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-app-bar color="transparent">
+    <v-avatar>
+      <v-img :src="require('@/assets/logo.png')" large></v-img>
+    </v-avatar>
+      <v-spacer></v-spacer>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </v-app-bar>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "AppBar"
+}
+</script>
+
+<style scoped>
+
+</style>
